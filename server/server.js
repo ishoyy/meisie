95c10ee4b4a98c799c3f1d072ade796714f0a085
Isha A <ishoyy.a@gmail.com>
2026-02-09 16:38:10 -0500

    fix port line

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 });
 
 // Start server
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
