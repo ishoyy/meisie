55571ec3f90abffbabde5596fb8601e01f18f5e9
Isha A <ishoyy.a@gmail.com>
2026-02-09 18:30:27 -0500

    change port

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
